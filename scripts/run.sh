24f5d6302e0792e09b4d314422a1a0d23bb9893c
OiSiS <oisis@o2.pl>
2016-11-04 22:59:30 +0100

    Add --keep-template to envtpl command.

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,22 +3,22 @@
 HOSTNAME=`cat /etc/hostname`
 
 if [[ $GRAPHITE_HOST && ${GRAPHITE_HOST-x} ]] || [[ $GRAPHITE_PORT && ${GRAPHITE_PORT-x} ]]; then
-    envtpl /etc/collectd/collectd.conf.d/write_graphite.conf.tpl
+    envtpl --keep-template /etc/collectd/collectd.conf.d/write_graphite.conf.tpl
 fi
 
 if [[ $INFLUXDB_HOST && ${INFLUXDB_HOST-x} ]] || [[ $INFLUXDB_PORT && ${INFLUXDB_PORT-x} ]]; then
-    envtpl /etc/collectd/collectd.conf.d/write_influxdb.conf.tpl
+    envtpl --keep-template /etc/collectd/collectd.conf.d/write_influxdb.conf.tpl
 fi
 
 if [[ $OPENTSDB_ADDR && ${OPENTSDB_ADDR-x} ]] || [[ $OPENTSDB_PORT && ${OPENTSDB_PORT-x} ]]; then
-    envtpl /etc/collectd/collectd.conf.d/tsdb.conf.tpl
+    envtpl --keep-template /etc/collectd/collectd.conf.d/tsdb.conf.tpl
 fi
 
 if [[ $LOGSTASH_HOST && ${LOGSTASH_HOST-x} ]] || [[ $LOGSTASH_PORT && ${LOGSTASH_PORT-x} ]]; then
-    envtpl /etc/collectd/collectd.conf.d/write_logstash.conf.tpl
+    envtpl --keep-template /etc/collectd/collectd.conf.d/write_logstash.conf.tpl
 fi
 
 # Prepare Collectd config file
-envtpl /etc/collectd/collectd.conf.tpl
+envtpl --keep-template /etc/collectd/collectd.conf.tpl
 
 /usr/sbin/collectd -C /etc/collectd/collectd.conf -f
